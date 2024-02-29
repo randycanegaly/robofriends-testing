5a305819cdf0765e94b5208ebb8dd87eff9069e3
randycanegaly <canegaly@gmail.com>
2024-02-28 20:37:51 -0500

    committing lost track of changes from ZTM Junior testing module

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -3,5 +3,7 @@ import { shallow } from 'enzyme';
 import Card from './Card';
 
 it('renders without crashing', () => {
+  expect.assertions(1);
+  // expect (shallow(<Card/>).length).toEqual(1);
   expect(shallow(<Card/>)).toMatchSnapshot();
 });
